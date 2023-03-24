27d729f281b4e17d41eb994ec9a37086de50e5cf
iwthu <102432185+iwthu@users.noreply.github.com>
2023-03-24 13:31:53 +0200

    first upload

---
+++ main.py
@@ -0,0 +1,121 @@
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import random
+
+app = Flask(__name__)
+
+##Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+##Cafe TABLE Configuration
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/random', methods=["GET"]) # can delete 'get' bc its allowed by default
+def get_random_cafe():
+    # Apparently this is the quickest way to get a random row from a database that may become large / Scalability
+    # Firstly, get the total number of rows in the database
+    row_count = Cafe.query.count()
+    # Generate a random number for skipping some records
+    random_offset = random.randint(0, row_count-1)
+    # Return the first record after skipping random_offset rows
+    random_cafe = Cafe.query.offset(random_offset).first()
+    # return jsonify(cafe = {"name": random_cafe.name,
+    #             "map_url": random_cafe.map_url,
+    #             "img_url": random_cafe.img_url,
+    #             "location": random_cafe.location,
+    #             "seats": random_cafe.seats,
+    #             "has_toilet": random_cafe.has_toilet,
+    #             "has_wifi": random_cafe.has_wifi,
+    #             "has_sockets": random_cafe.has_sockets,
+    #             "can_take_calls": random_cafe.can_take_calls,
+    #             "coffee_price": random_cafe.coffee_price})
+    return jsonify(cafe=random_cafe.to_dict())
+
+## HTTP GET - Read Record
+
+@app.route("/all")
+def all_cafes():
+    all_cafes = Cafe.query.all()
+    all_cafes_but_list = [cafe.to_dict() for cafe in all_cafes]
+    return jsonify(cafes=all_cafes_but_list)
+
+@app.route("/search")
+def get_cafe_at_location():
+    query_location = request.args.get("loc")
+    cafe = Cafe.query.filter_by(location=query_location).first()
+    if cafe:
+        return jsonify(cafe=cafe.to_dict())
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
+
+## HTTP POST - Create Record
+
+@app.route("/add", methods=["GET","POST"])
+def add_new_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url", ""), # provide a default value
+        location=request.form.get("loc", ""),
+        has_sockets=True if request.form.get("sockets") == "True" else False,
+        has_toilet=True if request.form.get("toilet") == "True" else False,
+        has_wifi=True if request.form.get("wifi") == "True" else False,
+        can_take_calls=True if request.form.get("calls") == "True" else False,
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price")
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
+
+## HTTP PUT/PATCH - Update Record
+
+@app.route("/update-price/<cafe_id>", methods=["PATCH"])
+def update_price(cafe_id):
+    cafe = Cafe.query.get(cafe_id)
+    new_price = request.args.get("new_price")
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={"success": f"Successfully updated the price to {new_price}."})
+    else:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
+## HTTP DELETE - Delete Record
+
+@app.route("/report-closed/<cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    cafe = Cafe.query.get(cafe_id)
+    if request.args.get("api_key") == "TopSecretAPIKey":
+        if cafe:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={"success": "Successfully deleted cafe."})
+        else:
+            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
+    else:
+        return jsonify(error={"Forbidden": "You dont have permission to perform this action. Make sure you are using correct api_key."})
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
